8d50b8f25a33064079460788f98ab8544ced0acc
ruslanmerdeev <r.merdeev@haulmont.com>
2022-06-27 14:18:17 +0400

    Modified the generics

--- src/main/kotlin/com/merdeev/kotlin/generics/Generics.kt
+++ src/main/kotlin/com/merdeev/kotlin/generics/Generics.kt
@@ -8,39 +8,46 @@ fun doGenerics() {
     println("genericClass.set(Class())").also { genericClass.set(Class()) }
     println("genericClass.set(SubClass())").also { genericClass.set(SubClass()) }
     println("genericClass.get()").also { println("${genericClass.get()}") }
-    println("genericClass.get().plus(4)").also { println("${genericClass.get()!!.plus(4)}") }
+    println("genericClass.get() + 4").also { println("${genericClass.get()!! + 4}") }
 
-//    val anotherGenericClass: GenericClass<Class> = GenericClass<SubClass>()           // can't do so
+//    val castedGenericClass: GenericClass<Class> = GenericClass<SubClass>()                // can't do so
+
+    val anotherGenericClass = GenericClass<AnotherClass>().also { println("create anotherGenericClass = GenericClass<AnotherClass>()") }
+    println("anotherGenericClass.set(AnotherClass())").also { anotherGenericClass.set(AnotherClass()) }
+    println("anotherGenericClass.get()").also { println("${anotherGenericClass.get()}") }
 
     val inClass = InClass<Class>().also { println("create inClass = InClass<Class>()") }
     println("inClass.set(SubClass())").also { inClass.set(SubClass()) }
-//    println("inClass.get()").also { println("${inClass.get()}") }
+//    println("inClass.get()").also { println("${inClass.get()}") }                         // can't do so
 
-//    val anotherInClass: InClass<Class> = InClass<SubClass>()                          // can't do so
+//    val castedInClass: InClass<Class> = InClass<SubClass>()                               // can't do so
 
     val outClass = OutClass<Class>().init(Class::class.java).also { println("create outClass = OutClass<Class>()") }
-//    println("outClass.set(Class())").also { outClass.set(Class()) }
+//    println("outClass.set(Class())").also { outClass.set(Class()) }                       // can't do so
     println("outClass.get()").also { println("${outClass.get()}") }
-    println("outClass.get().plus(4)").also { println("${outClass.get()!!.plus(4)}") }
+    println("outClass.get() + 4").also { println("${outClass.get()!! + 4}") }
 
-    val anotherOutClass: OutClass<Class> = OutClass<SubClass>().init(SubClass::class.java).also { println("create anotherOutClass = OutClass<SubClass>()") }
-    anotherOutClass.init(AnotherSubClass::class.java).also { println("init anotherOutClass with AnotherSubClass") }
-    println("anotherOutClass.get()").also { println("${anotherOutClass.get()}") }
+    val castedOutClass: OutClass<Class> = OutClass<SubClass>().init(SubClass::class.java).also { println("create castedOutClass = OutClass<SubClass>()") }
+    castedOutClass.init(AnotherSubClass::class.java).also { println("init castedOutClass with AnotherSubClass") }
+    println("castedOutClass.get()").also { println("${castedOutClass.get()}") }
 
     val subclassedOutClass: OutClass<SubClass> = OutClass<SubClass>().init(AnotherSubClass::class.java).also { println("create subclassedOutClass = OutClass<SubClass>()") }
     println("subclassedOutClass.get()").also { println("${subclassedOutClass.get()}") }
-//    println("subclassedOutClass.get().minus(4)").also { println("${subclassedOutClass.get()!!.minus(4)}") }       // throws ClassCastException
+//    println("subclassedOutClass.get() - 4").also { println("${subclassedOutClass.get()!! - 4}") }         // throws ClassCastException
 
-    val outBoundedClass = OutBoundedClass<Class>().init(Class::class.java).also { println("create outBoundedClass = OutBoundedClass<Class>()") }
-    println("outBoundedClass.get()").also { println("${outBoundedClass.get()}") }
-    println("outBoundedClass.get().plus(4)").also { println("${outBoundedClass.get()!!.plus(4)}") }
+    val anotherOutClass= OutClass<Class>().init(AnotherClass::class.java).also { println("create anotherOutClass = OutClass<AnotherClass>()") }
+    println("anotherOutClass.get()").also { println("${anotherOutClass.get()}") }
+//    println("anotherOutClass.get() + 4").also { println("${anotherOutClass.get()!! + 4}") }               // throws ClassCastException
 
-    val anotherBoundedOutClass: OutBoundedClass<Class> = OutBoundedClass<SubClass>().init(SubClass::class.java).also { println("create anotherBoundedOutClass = OutClass<SubClass>()") }
-    anotherBoundedOutClass.init(AnotherSubClass::class.java).also { println("init anotherOutClass with AnotherSubClass") }
-    println("anotherBoundedOutClass.get()").also { println("${anotherBoundedOutClass.get()}") }
+    val boundedClass = BoundedClass<Class>().also { println("create boundedClass = OutBoundedClass<Class>()") }
+    println("boundedClass.set(Class())").also { boundedClass.set(Class()) }
+    println("boundedClass.set(SubClass())").also { boundedClass.set(SubClass()) }
+    println("boundedClass.get()").also { println("${boundedClass.get()}") }
+    println("boundedClass.get() + 4").also { println("${boundedClass.get()!! + 4}") }
 
-    val subclassedBoundedOutClass: OutBoundedClass<SubClass> = OutBoundedClass<SubClass>().init(AnotherSubClass::class.java).also { println("create subclassedBoundedOutClass = OutClass<SubClass>()") }
-    println("subclassedBoundedOutClass.get()").also { println("${subclassedBoundedOutClass.get()}") }
+//    val castedBoundedClass: BoundedClass<Class> = BoundedClass<SubClass>()                // can't do so
+
+//    val anotherBoundedClass: BoundedClass<AnotherClass> = BoundedClass<AnotherClass>()    // can't do so
 }
 
 open class Class(var value: Int = 10) {
@@ -69,6 +76,13 @@ class AnotherSubClass(private var addValue: Int = 3) : Class() {
 
     override fun toString(): String {
         return "$value $addValue"
+    }
+}
+
+class AnotherClass {
+
+    override fun toString(): String {
+        return "AnotherClass()"
     }
 }
 
@@ -107,25 +121,21 @@ class OutClass<out T> {
     }
 
     fun get(): T? {
-        return list.getOrNull(list.size-1)
+        return list.getOrNull(list.size - 1)
     }
 
 //    fun set(arg: T) {}            // can't do so cause of 'out T'
 }
 
-class OutBoundedClass<out T : Class> {
+class BoundedClass<T : Class> {
 
     private var list: MutableList<T> = mutableListOf()
-
-    fun init(clazz: java.lang.Class<*>): OutBoundedClass<T> {
-        val newOne = clazz.getDeclaredConstructor().newInstance() as T
-        list.add(newOne)
-        return this
-    }
 
     fun get(): T? {
         return list.getOrNull(list.size-1)
     }
 
-//    fun set(arg: T) {}            // can't do so cause of 'out T'
+    fun set(arg: T) {
+        list.add(arg)
+    }
 }
