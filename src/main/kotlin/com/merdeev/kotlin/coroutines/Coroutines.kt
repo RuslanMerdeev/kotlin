6122038e85faa4b835e6e9115da544fcf486e8db
ruslanmerdeev <r.merdeev@haulmont.com>
2022-06-28 18:11:38 +0400

    Added the coroutineScopes to coroutines

--- src/main/kotlin/com/merdeev/kotlin/coroutines/Coroutines.kt
+++ src/main/kotlin/com/merdeev/kotlin/coroutines/Coroutines.kt
@@ -10,33 +10,37 @@ fun doCoroutines() {
     runBlocking {
         println("start runBlocking")
 
-        launch("launch1", 600)
-
+        val job = launch("launch1", 600)
         launch("launch2", 500)
-
         coroutineScope(
             "coroutineScope1",
             400,
             { launch("launch3", 300) },
+            { coroutineScope("coroutineScope2", 200) },
+            { coroutineScope("coroutineScope3", 100) },
+            { launch("launch3", 200) },
         )
+        launch("launch4", 300)
 
-        coroutineScope("coroutineScope2", 100)
+        coroutineScope("coroutineScope4", 100)
 
+        println("cancel launch1")
+        job.cancelAndJoin()
         println("end runBlocking")
     }
     println("after runBlocking")
 }
 
-private fun CoroutineScope.launch(name: String, timeout: Long) {
+private fun CoroutineScope.launch(name: String, timeout: Long): Job {
     println("before $name")
-    launch {
+    return launch {
         println("start $name")
         delay(timeout)
         println("end $name")
     }
 }
 
-private suspend fun coroutineScope(name: String, timeout: Long, vararg subs: () -> Unit) {
+private suspend fun coroutineScope(name: String, timeout: Long, vararg subs: suspend () -> Unit) {
     println("before $name")
     coroutineScope {
         println("start $name")
